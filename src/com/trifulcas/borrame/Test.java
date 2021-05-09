2175a6c4f87dcbecbd15c000ae3ae8f463f441e9
Juan Pablo Fuentes <51440192+juanpablofuentes@users.noreply.github.com>
2021-05-09 19:54:22 +0200

    a

--- src/com/trifulcas/borrame/Test.java
+++ src/com/trifulcas/borrame/Test.java
@@ -3,11 +3,13 @@ package com.trifulcas.borrame;
 public class Test {
 
 	public static void main(String[] args) {
+		
+		
 		System.out.println("adios");
-		System.out.println("adios");
+		
 
 	}
-	public static void foo() {
+	public static void foos() {
 		System.out.println("Booo");
 	}
 
