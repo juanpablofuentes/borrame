7a0b25a95fcf3961e81cd3321d074ab4d56ef310
Juan Pablo <incidencias@gmail.com>
2021-05-10 09:43:30 +0200

    Cambio tonto

--- src/com/trifulcas/borrame/Test.java
+++ src/com/trifulcas/borrame/Test.java
@@ -5,10 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("adios");
-		
 
 	}
 	public static void foos() {
+		System.out.println("Booo");
 		System.out.println("Booo");
 	}
 
